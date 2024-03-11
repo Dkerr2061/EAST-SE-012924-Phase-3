13db827e72d0ea78c14340f70634c85e75c009ed
David Kerr <kerrky@gmail.com>
2024-03-11 12:19:37 -0400

    Updated lecture

--- lib/app.py
+++ lib/app.py
@@ -1,3 +1,44 @@
 import ipdb
 
-# write your code here
+print("Hello Flatiron! Class is in session!")
+
+def add(num1, num2):
+  if(type(num1) in [int, float]) and (type(num2) in [int, float]):
+      return num1 + num2
+  else:
+     raise Exception
+  
+def subtract(num1, num2):
+   if(type(num1) in [int, float]) and (type(num2) in [int, float]):
+      return num1 - num2
+   else:
+      raise Exception
+   
+def multiply(num1, num2):
+   if(type(num1) in [int, float]) and (type(num2) in [int, float]):
+      return num1 * num2
+   else:
+      raise Exception
+   
+def divide(num1, num2):
+   if(type(num1) in [int, float]) and (type(num2) in [int, float]) and (num2 != 0):
+      return num1 / num2
+   else:
+      raise Exception
+   
+def calculator(operator, num1, num2):
+   if(operator == '+'):
+      return add(num1, num2)
+   elif(operator == '-'):
+      return subtract(num1, num2)
+   elif(operator == '*'):
+      return multiply(num1, num2)
+   elif(operator == '/'):
+      return divide(num1, num2)
+   else:
+      raise Exception
+   
+
+def print_greeting_loop(greeting):
+   for char in greeting:
+      print(char)
